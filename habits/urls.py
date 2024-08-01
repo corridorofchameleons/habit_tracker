b10aeb6427c81c5eca931b434bb19c2b86c7a979
speedfreak <gdramze@gmail.com>
2024-08-02 00:49:20 +0300

    add my habits

--- habits/urls.py
+++ habits/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import HabitCreateAPIView, HabitUpdateAPIView, HabitDestroyAPIView
+from habits.views import HabitListCreateAPIView, HabitUpdateAPIView, HabitDestroyAPIView, MyHabitsListView
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
     path('<int:pk>/update/', HabitUpdateAPIView.as_view(), name='habit_update'),
     path('<int:pk>/delete/', HabitDestroyAPIView.as_view(), name='habit_delete'),
-    path('', HabitCreateAPIView.as_view(), name='habit_create'),
+    path('', HabitListCreateAPIView.as_view(), name='habit_create'),
+    path('my_habits/', MyHabitsListView.as_view(), name='my_habits'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -2,13 +2,16 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from habits.models import Habit
+from habits.pagination import MyPaginator
 from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
-class HabitCreateAPIView(generics.CreateAPIView):
+class HabitListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = HabitSerializer
     permission_classes = IsAuthenticated,
+    queryset = Habit.objects.filter(is_public=True)
+    pagination_class = MyPaginator
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -24,3 +27,11 @@ class HabitDestroyAPIView(generics.DestroyAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
     permission_classes = IsOwner,
+
+
+class MyHabitsListView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = MyPaginator
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
